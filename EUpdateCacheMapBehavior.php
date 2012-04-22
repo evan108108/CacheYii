668f444b1d7a65ee46c5925ba41f529e66389b0f
Evan Frohlich <evan.frohlich@controlgroup.com>
2012-04-22 15:25:16 -0400

    Added Const for CACHE_MAP_NAME to EUpdateCacheMapBehavior

--- EUpdateCacheMapBehavior.php
+++ EUpdateCacheMapBehavior.php
@@ -1,18 +1,20 @@
 <?php
 class EUpdateCacheMapBehavior extends CActiveRecordBehavior
 {
+  Const CACHE_MAP_NAME = 'modelUpdateMap';
+
   public $cacheExp = 0;
   public $modelName;
 
   public function afterSave($event)
   {
-    $cacheMap = Yii::app()->cache->get('modelUpdateMap');
+    $cacheMap = Yii::app()->cache->get(self::CACHE_MAP_NAME);
     if($cacheMap === false)
       $cacheMap = array();
 
     $cacheMap[$this->modelName] = time();
     
-    Yii::app()->cache->set('modelUpdateMap', $cacheMap, $this->cacheExp);
+    Yii::app()->cache->set(self::CACHE_MAP_NAME, $cacheMap, $this->cacheExp);
     
     return parent::afterSave($event);
   }
